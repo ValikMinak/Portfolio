8a48d3b375ade194ff9ea56a9ac459298d5d03bc
ValikMinak <valikminak@gmail.com>
2020-09-22 12:05:30 +0300

    Updates

---
+++ precache-manifest.7f06876f0c5a59a03a21a97de3119cf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843ebcf4aa15e8c82c131f2851888a55",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "748899b12f7b2f5a3b70",
+    "url": "/Portfolio/static/css/main.a4de08db.chunk.css"
+  },
+  {
+    "revision": "5e0bf405872413a810b3",
+    "url": "/Portfolio/static/js/2.0cbabee3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.0cbabee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748899b12f7b2f5a3b70",
+    "url": "/Portfolio/static/js/main.52ace24d.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "09a89c03bae2addfd6776abed168adb9",
+    "url": "/Portfolio/static/media/nightinTokyo.09a89c03.woff"
+  }
+]);
